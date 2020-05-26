2514e5923a4ea5f0e18fb4f789a53cf9c6d3430d
Muhammad Qadri <221709860@stis.ac.id>
2020-05-26 23:08:24 +0700

    change title, api_url

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@
 		modal.find('.p-demo').html(linkDemo);
 	})
 
-	const URL_API = "http://api.mqad21.my.id/github/portfolio.php"
+	const URL_API = "https://api.mqad21.my.id/github/portfolio.php"
 
 	$.ajax({
 		url: URL_API,
